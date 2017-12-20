b45261ae387d0d268415a60b035d40f1917a7a84
92wmm <6492178>
2017-12-20 16:04:39 +0800

    1.初始化开始写demo

---
+++ payment-server/src/main/java/w/m/vker/demo/paymentserver/PaymentServerApplication.java
@@ -0,0 +1,12 @@
+package w.m.vker.demo.paymentserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentServerApplication.class, args);
+	}
+}
---
+++ zuul-server/src/main/java/w/m/vker/demo/zuulserver/ZuulServerApplication.java
@@ -0,0 +1,12 @@
+package w.m.vker.demo.zuulserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulServerApplication.class, args);
+	}
+}
