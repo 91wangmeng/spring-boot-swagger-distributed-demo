2074e01942e5bc364b9298e81fe394ec97252bd7
wang_mm <Qq6492178>
2018-06-26 13:59:19 +0800

    1.修复pom异常和springboot版本更新导致的问题

---
+++ zuul-server/src/main/java/w/m/vker/demo/zuulserver/config/SwaggerConfig.java
@@ -0,0 +1,45 @@
+package w.m.vker.demo.zuulserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * 浙江卓锐科技股份有限公司 版权所有 © Copyright 2017<br>
+ * 说明: <br>
+ * 项目名称: control-api <br>
+ * 创建日期: 2017年09月06日 12:21 <br>
+ * 作者: <a href="6492178@gmail.com">汪萌萌</a>
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("分布式购物系统")
+                .description("购物系统接口文档说明")
+                .termsOfServiceUrl("http://localhost:8081")
+                .contact(new Contact("vker", "", "6492178@gmail.com"))
+                .version("1.0")
+                .build();
+    }
+
+    @Bean
+    UiConfiguration uiConfig() {
+        return new UiConfiguration(null, "list", "alpha", "schema",
+                UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS, false, true, 60000L);
+    }
+}
